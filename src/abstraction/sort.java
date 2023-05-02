17ef2f62e5142758a78fc9927d13f3f1cb5a093f
Chavda Himanshu H <himanshuisherenow@gmail.com>
2023-05-02 22:49:32 +0530

    okay this is sorting which i should not learn now

---
+++ src/abstraction/sort.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class sort {
+    public static void main(String[] args) {
+        int arr[]={5,9,6,2,6};
+ Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
